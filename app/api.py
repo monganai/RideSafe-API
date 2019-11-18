a8f3aa0b1af4ea7639ccf5cf1d18d5de5ea4541c
monganai <monganai@tcd.ie>
2019-11-18 16:39:51 +0000

    minor tweaks

--- app/api.py
+++ app/api.py
@@ -1,19 +1,27 @@
 from app import app,db
 from ddtrace import patch_all,tracer
 import logging
+import os
+from app.models import User, Post
+
+
 patch_all()
 
+#Docker tracer configuration
 #tracer.configure(
 #    hostname='datadog-agent',
 #    port=8126,
 #)
+
+
+#kubernetes tracer configuration.
 
 tracer.configure(
     hostname=os.environ['DD_AGENT_HOST'],
     port=os.environ['DD_TRACE_AGENT_PORT'],
 )
 
-from app.models import User, Post
+
 
 @app.shell_context_processor
 def make_shell_context():
