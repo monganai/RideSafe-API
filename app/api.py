52220b567875d1b4e7c8004695c0ede1b0ae4754
monganai <monganai@tcd.ie>
2019-12-11 20:59:38 +0000

    minor changes

--- app/api.py
+++ app/api.py
@@ -16,6 +16,7 @@ tracer.configure(
     #port=os.environ['DD_TRACE_AGENT_PORT'],
 )
 
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'CrashLocationPoint' : CrashLocationPoint, 'CrashDataPoint' : CrashDataPoint}
