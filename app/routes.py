229b2f894338867b3de5aeea897a9018acffdf92
monganai <monganai@tcd.ie>
2020-05-17 23:11:19 +0100

    scatter chart on index page, specified allowed methods for the endpoints, code refactor

--- app/routes.py
+++ app/routes.py
@@ -25,33 +25,49 @@ FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
 logging.basicConfig(format = FORMAT)
 log = logging.getLogger(__name__)
 log.level = logging.INFO
-logging.basicConfig()
+logging.basicConfig(format = FORMAT)
 app.logger.addHandler(logging.StreamHandler())
 app.logger.setLevel(logging.INFO)
 
 # ############## User Interface Endpoints #####################
 
-# Index page
-@app.route('/')
-@app.route('/index')
-# @login_required
+API_KEY = os.environ["DD_PROFILING_API_KEY"]
+clientToken = os.environ["DD_CLIENT_TOKEN"]
+applicationId = os.environ["DD_APPLICATION_ID"]
+host = os.environ["DD_AGENT_HOST"]
+redis_port = 30001
+
+
+
+
+############ Backend Methods ###############################
 @tracer.wrap()
-def index():
-    API_KEY = os.environ["DD_PROFILING_API_KEY"]
-    clientToken = os.environ["DD_CLIENT_TOKEN"]
-    applicationId = os.environ["DD_APPLICATION_ID"]
-    
-    
-    
-    return render_template('index.html', title = 'Home', applicationId = applicationId, clientToken = clientToken)
+def getCrashPoints():
+    glist = []
+    rlist = []
+    points = CrashLocationPoint.query.all()
+    for point in points:
+        glist.append(point.gforce)
+        rlist.append(point.longitude)
+        
+    return glist, rlist
+
+############################################################
 
 
-@app.route('/gallery')
+# Index page
+@app.route('/', methods = ['GET'])
+@tracer.wrap()
+def index(): 
+    glist, rlist = getCrashPoints()    
+    return render_template('index.html', title = 'Home', applicationId = applicationId, clientToken = clientToken, glist = glist, rlist = rlist)
+
+
+@app.route('/gallery', methods = ['GET'])
 @tracer.wrap()
 def gallery():
     log.info('Gallery Accessed')
-    return render_template('gallery.html', title = 'App Gallery')
-
+    return render_template('gallery.html', title = 'App Gallery', applicationId = applicationId, clientToken = clientToken)
 
 
 @app.route('/login', methods = ['GET', 'POST'])
@@ -74,7 +90,7 @@ def login():
     return render_template('login.html', title = 'Sign In', form = form)
 
 
-@app.route('/logout')
+@app.route('/logout', methods = ['GET'])
 def logout():
     logout_user()
     return redirect(url_for('index'))
@@ -95,24 +111,8 @@ def register():
 
 # ################### charts ##################################
 
-@app.route("/line_chart")
-def line_chart():
-    clientToken = os.environ["DD_CLIENT_TOKEN"]
-    applicationId = os.environ["DD_APPLICATION_ID"]
-    legend = 'Temperatures'
-    temperatures = [73.7, 73.4, 73.8, 72.8, 68.7, 65.2, 
-                    61.8, 58.7, 58.2, 58.3, 60.5, 65.7, 
-                    70.2, 71.4, 71.2, 70.9, 71.3, 71.1]
-    times = ['12:00PM', '12:10PM', '12:20PM', '12:30PM', '12:40PM', '12:50PM', 
-             '1:00PM', '1:10PM', '1:20PM', '1:30PM', '1:40PM', '1:50PM', 
-             '2:00PM', '2:10PM', '2:20PM', '2:30PM', '2:40PM', '2:50PM']
-    return render_template('line_chart.html', values = temperatures, labels = times, legend = legend, applicationId = applicationId, clientToken = clientToken)
-
-
-@app.route("/chart")
+@app.route("/chart/wip", methods = ['GET'])
 def chart():
-    clientToken = os.environ["DD_CLIENT_TOKEN"]
-    applicationId = os.environ["DD_APPLICATION_ID"]
     glist = []
     rlist = []
     points = CrashLocationPoint.query.all()
@@ -121,14 +121,14 @@ def chart():
         rlist.append(point.longitude)
     legend = 'Gforce vs Rotation'
 
-    return render_template('line_chart.html', values = glist, labels = rlist, legend = legend, applicationId = applicationId, clientToken = clientToken)
+    return render_template('scatter.html', values = glist, labels = rlist, legend = legend, applicationId = applicationId, clientToken = clientToken)
 
 
 # ################## API Endpoints #############################
 
 
 # Load training Data into Database
-@app.route('/loadtd')
+@app.route('/loadtd', methods = ['GET'])
 def loadtd():
     CrashDataPoint.query.delete()
     basedir = os.path.abspath(os.path.dirname(__file__))
@@ -151,9 +151,12 @@ def loadtd():
 @app.route('/crashPoint/redis', methods = ['GET'])
 @tracer.wrap()
 def get_all_pointsre():
+    dict = {}
+    i = 0
+    s1 = 'latitude '
+    s2 = 'longitude '
+    points = CrashLocationPoint.query.all()
     try:
-        host = os.environ["DD_AGENT_HOST"]
-        redis_port = 30001
         r = redis.Redis(host = host, port = redis_port)
         r.set("crashpoint-redis", "working")
         msg = r.keys() 
@@ -165,19 +168,11 @@ def get_all_pointsre():
         root_span.set_tag('error', 'true')
         root_span.set_tag('error.message', 'connection to redis failed')
 
-    dict = {}
-    i = 0
-    s1 = 'latitude '
-    s2 = 'longitude '
-    points = CrashLocationPoint.query.all()
     for point in points:
-        lat = point.latitude
-        long = point.longitude
-        #print(lat)
         t1 = s1 + str(i)
         t2 = s2 + str(i)
-        dict[t1] = lat
-        dict[t2] = long
+        dict[t1] = point.latitude
+        dict[t2] = point.longitude
         i = i + 1
         r.set("key", "value")
         msg = r.get("key") 
@@ -196,13 +191,10 @@ def get_all_points():
     s2 = 'longitude '
     points = CrashLocationPoint.query.all()
     for point in points:
-        lat = point.latitude
-        long = point.longitude
-        #print(lat)
         t1 = s1 + str(i)
         t2 = s2 + str(i)
-        dict[t1] = lat
-        dict[t2] = long
+        dict[t1] = point.latitude
+        dict[t2] = point.longitude
         i = i + 1
     jdict = json.dumps(dict)
 
